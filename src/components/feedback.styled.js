760871c629388dc0e33e0ad2377ff21a2e979fbb
Deathshamhd <122151628+Deathshamhd@users.noreply.github.com>
2023-10-27 01:09:12 +0300

    save

--- src/components/feedback.styled.js
+++ src/components/feedback.styled.js
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 export const MainContainer = styled.div`
     margin:10px;
 `
-export const Buttons = styled.button`
+export const Buttns = styled.button`
     border: 1px solid red;
-    background-color: transparent;
+    background-color: orange;
+    color:lightblue;
     border-radius: 5px;
     margin-right:5px;
     font-size:12px;
